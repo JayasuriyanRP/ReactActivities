9e24c7182827c94ea1e5d180fdc40ee1fa93b4b5
jayasuriyan <r.p.jayasuriyanece@outlook.com>
2021-05-16 18:39:47 +0530

    Fix for missing uuid reference

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -10,7 +10,7 @@ export default observer(function ActivityDashboard() {
     const {loadActivities, activityRegistry} = activityStore;
     useEffect(() => {
         if(activityRegistry.size <= 1) loadActivities();
-    }, [activityRegistry.size, activityStore])
+    }, [activityRegistry.size, activityStore, loadActivities])
 
     if (activityStore.loadingInitial) return <LoadingComponent content="Loading App" />
 

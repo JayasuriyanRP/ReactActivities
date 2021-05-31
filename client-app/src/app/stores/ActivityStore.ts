1f6ef5b0f7d457fdfebab95b85570c51bb4d7aaf
Jayasuriyan Rp <jayasuriyan.rp@in.abb.com>
2021-05-31 07:41:18 +0530

    End of Section 9

--- client-app/src/app/stores/ActivityStore.ts
+++ client-app/src/app/stores/ActivityStore.ts
@@ -1,6 +1,8 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+
+
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
@@ -14,6 +16,16 @@ export default class ActivityStore {
 
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity)=>{
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key:string]: Activity[]})
+        )
     }
 
     loadActivities = async () => {
